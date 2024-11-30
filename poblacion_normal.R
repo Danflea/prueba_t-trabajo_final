3f41767686095b94aeb2fc9080ffcdb2e84b54b7
Danilo <20220763@lamolina.edu.pe>
2024-11-30 00:42:34 -0500

    Nueva población

--- poblacion_normal.R
+++ poblacion_normal.R
@@ -3,10 +3,13 @@ library(readxl)
 BD <- read_excel("BD_2S ENLA muestral 2023.xlsx")
 View(BD)
 
+library(readxl)
+BD <- read_excel("C:/Users/frusc/Desktop/Análisis Estadistico - Danilo/Trabajos_encargados/RMD-Trabajo_anaalisis-estadistico/BD_2S ENLA muestral 2023.xlsx")
+
 library(magrittr)
 library(dplyr)
 
-data_ayacucho <- BD %>% filter(departamento == "AYACUCHO") %>% select(c(
+data_ayacucho <- BD %>% filter(departamento == "AYACUCHO") %>% select(c("sexo",
   "area",                     # Zona donde viven (rural o urbana)
   "M500_EM_2S_2023_CT",       # Puntaje de comprensión lectora
   "M500_EM_2S_2023_MA",       # Puntaje de matemáticas
@@ -15,7 +18,6 @@ data_ayacucho <- BD %>% filter(departamento == "AYACUCHO") %>% select(c(
 
 any(is.na(data_ayacucho))
 data_ayacucho_limpio <- na.omit(data_ayacucho)
-
 
 ############################
 ############### Rural ######
@@ -54,38 +56,9 @@ hist(nota_m, main = "Histograma de Nota Matemáticas", xlab = "Puntajes")
 
 
 
-##################
-#### Urbana ###### 
-##################
 
 
-data_urbano <- data_ayacucho_limpio %>% filter(area == "Urbana") %>% select(-area)
 
-############### Nota matemáticas ############################
-
-nota_m_urb <- data_urbano$M500_EM_2S_2023_MA # se tiene que extraer como vector numerico
-shapiro.test(nota_m_urb)
-
-any(is.na(nota_m_urb))
-# Q-Q Plot
-qqnorm(nota_m_urb)
-qqline(nota_m_urb, col = "red")
-
-# Histograma
-hist(nota_m_urb, main = "Histograma de Nota Matemáticas", xlab = "Puntajes")
-
-##  quitar valores extremos 
-z_scores_m_urb <- scale(nota_m_urb)
-extremos_m_urb <- which(abs(z_scores_m_urb)>3)
-data_extremos_m_urb <- nota_m[extremos_m_urb]
-print(data_extremos_m_urb)
-
-nota_m_sin_extremos_urb <- nota_m_urb[-extremos_m_urb]
-
-shapiro.test(nota_m_sin_extremos_urb)  # cumple normalidad 
-qqnorm(nota_m_sin_extremos_urb)
-qqline(nota_m_sin_extremos_urb, col = "red")
-hist(nota_m_sin_extremos_urb, main = "Histograma de Nota Matemáticas", xlab = "Puntajes")
 
 
 
