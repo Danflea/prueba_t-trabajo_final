acdaa436938e71992374411f4230b65a91bca456
Danilo <20220763@lamolina.edu.pe>
2024-11-30 20:32:33 -0500

    normalidad y homogeneidad de la poblacion

--- poblacion_normal.R
+++ poblacion_normal.R
@@ -3,34 +3,17 @@ library(readxl)
 BD <- read_excel("BD_2S ENLA muestral 2023.xlsx")
 View(BD)
 
-library(readxl)
-BD <- read_excel("C:/Users/frusc/Desktop/Análisis Estadistico - Danilo/Trabajos_encargados/RMD-Trabajo_anaalisis-estadistico/BD_2S ENLA muestral 2023.xlsx")
-
 library(magrittr)
 library(dplyr)
 
-data_ayacucho <- BD %>% filter(departamento == "AYACUCHO") %>% select(c("sexo",
-  "area",                     # Zona donde viven (rural o urbana)
-  "M500_EM_2S_2023_CT",       # Puntaje de comprensión lectora
-  "M500_EM_2S_2023_MA",       # Puntaje de matemáticas
-  "M500_EM_2S_2023_CS"        # Puntaje de ciencias sociales
-))
+data_ayacucho_nuevo <- BD %>% 
+  filter(departamento == "AYACUCHO", area == "Rural") %>% 
+  select(c("sexo", "M500_EM_2S_2023_MA")) %>% 
+  na.omit()
 
-any(is.na(data_ayacucho))
-data_ayacucho_limpio <- na.omit(data_ayacucho)
+shapiro.test(data_ayacucho_nuevo$M500_EM_2S_2023_MA)
 
-############################
-############### Rural ######
-############################
-
-
-data_rural <- data_ayacucho_limpio %>% filter(area=="Rural") %>% select(-area)
-
-
-############### Nota matemáticas ############################
-
-nota_m <- data_rural$M500_EM_2S_2023_MA # se tiene que extraer como vector numerico
-shapiro.test(nota_m)
+nota_m <- data_rural$M500_EM_2S_2023_MA # extraer las notas en forma de vector 
 
 # Q-Q Plot
 qqnorm(nota_m)
@@ -45,15 +28,50 @@ extremos_m <- which(abs(z_scores_m)>3)
 data_extremos_m <- nota_m[extremos_m]
 print(data_extremos_m)
 
-nota_m_sin_extremos <- nota_m[-extremos_m]
-
-shapiro.test(nota_m_sin_extremos)  # cumple normalidad 
-qqnorm(nota_m_sin_extremos)
-qqline(nota_m_sin_extremos, col = "red")
-hist(nota_m, main = "Histograma de Nota Matemáticas", xlab = "Puntajes")
-
+data_rural_sin <- data_rural[-extremos_m, ]
+shapiro.test(data_rural_sin$M500_EM_2S_2023_MA)  # cumple normalidad 
+qqnorm(data_rural_sin$M500_EM_2S_2023_MA)
+qqline(data_rural_sin$M500_EM_2S_2023_MA, col = "red")
+hist(data_rural_sin$M500_EM_2S_2023_MA, main = "Histograma de Nota Matemáticas", xlab = "Puntajes")
 
 
+### Población #####
+
+data_rural_sin
+summary(data_rural_sin$M500_EM_2S_2023_MA)
+
+#### Comprobando normalidad de los grupos 
+
+data_hombre <- data_rural_sin %>% filter(sexo == "Hombre") %>% select(2)
+shapiro.test(data_hombre$M500_EM_2S_2023_MA)   # cumple normalidad 
+
+N_h <- nrow(data_hombre)
+
+data_mujer <- data_rural_sin %>% filter(sexo == "Mujer") %>%  select(2)
+shapiro.test(data_mujer$M500_EM_2S_2023_MA)    # cumple normalidad 
+
+N_m <- nrow(data_mujer)
+
+# Estimar el tamaño de muestra para una población infinita
+
+n <- 160 # tamaño total de la muestra
+
+n_h <- round(n*N_h/N, 0) # tamaño de muestra de estrato masculino
+n_m <- round(n*N_m/N, 0) # tamaño de muestra de estrao femenino
+
+########### Muestra ##############
+
+library(TeachingSampling)
+set.seed(123)
+sam <- S.STSI(data_rural_sin$sexo, c(N_h, N_m), c(n_h, n_m))
+muestra <- data_rural_sin[sam,]
+View(muestra)
+
+muestra_hombre <- muestra %>% filter(sexo == "Hombre") %>% select(2)
+shapiro.test(muestra_hombre$M500_EM_2S_2023_MA)
+
+muestra_mujer <- muestra %>% filter(sexo == "Mujer") %>% select(2)
+shapiro.test(muestra_mujer$M500_EM_2S_2023_MA)
 
 
 
